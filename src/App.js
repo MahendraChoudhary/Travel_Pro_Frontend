1fb46926bbc47a7e731f6e870dc97244a044e404
Mahendra_Choudhary <mahendrajat072003@gmail.com>
2023-12-16 14:32:04 +0530

    added date selector for navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // import logo from './logo.svg';
 import React from 'react'
-import {Home} from './pages'
+import {Route, Routes} from 'react-router-dom'
+import {Home, SingleHotel} from './pages'
 import './App.css';
 
 function App() {
   return (
-    <Home />
-    // <h1>Hello world</h1>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path="/hotels/:name/:address/:id/reserve" element={<SingleHotel />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import { CategoryProvider } from './context';
+import { CategoryProvider} from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CategoryProvider>
-      <App />
-    </CategoryProvider>
+    <Router>
+      <CategoryProvider>
+        <App />
+      </CategoryProvider>
+    </Router>
   </React.StrictMode>
 );
 
